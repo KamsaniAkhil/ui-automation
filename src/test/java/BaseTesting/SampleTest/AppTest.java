3ae2c2a2440cb1d570b80714f1a838c2061943e4
KamsaniAkhil <akhilkamsani01@gmail.com>
2022-12-17 00:54:46 +0530

    WrittenHelperClass

--- src/test/java/BaseTesting/SampleTest/AppTest.java
+++ src/test/java/BaseTesting/SampleTest/AppTest.java
@@ -2,186 +2,300 @@ package BaseTesting.SampleTest;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.File;
-import java.util.Arrays;
-import java.util.Properties;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Calendar;
+import java.util.List;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.IAnnotationTransformer;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.ITestAnnotation;
+import org.testng.annotations.Test;
 
-import com.opera.core.systems.OperaDriver;
-import com.sampleTest.PropsHandler;
-
-
+import com.sampleTest.SampleHelperClass;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-//	public static String execExtn = "";
+public class AppTest implements IRetryAnalyzer, IAnnotationTransformer {
+
+	Logger logger = LoggerFactory.getLogger("DashboardTest");
+
+	public static String currentRunningMethodName = "";
+	private int retryCount = 0;
+	private int maxRetryCount = 1;
+	public static String username = System.getProperty("email", "v-arungopi@aims360.com");
+	public static String password = System.getProperty("password", "JR365@ims22!@");
+	protected String TestcaseId;
+	private String countNotification = null;
+	// Helper classes
+	SampleHelperClass webAppHelper = new SampleHelperClass();
+	
+	public static WebDriver driver;
+	Calendar cal = Calendar.getInstance();
+
+	// Getting present day
+	int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+
+	@BeforeClass
+	public void setup() {
+		webAppHelper.openBrowser("chrome");
+		webAppHelper.maximizeWindow();
+
+		logger.info("Navigating to the URL");
+		webAppHelper.navigateTo("https://dev-jr365.aims360runway.com");
+		webAppHelper.waitSimply(3);
+	}
+
+	@BeforeMethod
+	public void beforeMethod(Method method) {
+		currentRunningMethodName = method.getName();
+	}
+
+	@Override
+	public boolean retry(ITestResult result) {
+		if (retryCount < maxRetryCount) {
+			System.out.println("Retrying test " + result.getName() + " with status "
+					+ getResultStatusName(result.getStatus()) + " for the " + (retryCount + 1) + " time(s).");
+			retryCount++;
+
+			return true;
+		}
+		return false;
+	}
+
+	public void transform(ITestAnnotation testannotation, Class testClass, Constructor testConstructor,
+			Method testMethod) {
+		IRetryAnalyzer retry = testannotation.getRetryAnalyzer();
+
+		if (retry == null) {
+			testannotation.setRetryAnalyzer(AppTest.class);
+		}
+
+	}
+
+	public String getResultStatusName(int status) {
+		String resultName = null;
+		if (status == 1)
+			resultName = "SUCCESS";
+		else if (status == 2)
+			resultName = "FAILURE";
+		else if (status == 3)
+			resultName = "SKIP";
+		return resultName;
+
+	}
+
+	/* TestCaseID is C18994 */
+	@Test(enabled = true, priority = 1)
+	public void landingWelcomePage(){
+
+		webAppHelper.loginPage(username, password);
+		webAppHelper.waitForElementToBeLoadedByClassName("welcomeMessage");
+		// TestcaseId = "2";
+		logger.info("Getting display welcome message");
+		String welcome = webAppHelper.findElementByClassName("welcomeMessage").getText();
+
+		logger.info("Asserting welcome text");
+		Assert.assertEquals(welcome, "Welcome to AIMS360");
+
+		logger.info("getting logo position css value");
+		String logoPosition = webAppHelper.findElementByXPath("//div[contains(@class,'pt-4')]//child::img")
+				.getCssValue("vertical-align");
+
+		logger.info("Asserting Logo css value");
+		Assert.assertEquals(logoPosition, "middle");
+
+//		if (logoPosition.contains("middle")) {
 //
-//	public final static String CR_DRIVER_PATH = System.getProperty("webdriver.chrome.driver",
-//			"lib" + File.separator + "chromedriver" + "");
-	static Logger logger = LoggerFactory.getLogger(AppTest.class);
-	public static String username = System.getProperty("email", "shahin@aims360.com");
-	public static String password = System.getProperty("password", "M5ONteHNUT6bvV!#YWV3");
-	
-	public static String APP_BASE_URL = null;
-	public WebDriver driver = null;
-	public final static String FF_BROWSER = "firefox";
-	public final static String CR_BROWSER = "chrome";
-	public final static String IE_BROWSER = "ie";
-	public final static String IE_EDGE_BROWSER = "edge";
-	public final static String SF_BROWSER = "safari";
-	public final static String OP_BROWSER = "opera";
-	public final static String HU_BROWSER = "htmlunit";
-	public static boolean isWin = false;
-	public static boolean isLinux = false;
-	public static boolean isUnix = false;
-	public static boolean isMac = false;
-	public static String execExtn = "";
-	public static String defaultBrowser = "chrome";
-	static {
-		String osName = System.getProperty("os.name").toLowerCase();
-		System.out.println("os.name=" + osName);
-		if (osName.indexOf("win") != -1) {
-			isWin = true;
-			execExtn = ".exe";
-			defaultBrowser = "ie";
-		} else if (osName.indexOf("nix") != -1) {
-			isUnix = true;
-			defaultBrowser = "firefox";
-		} else if (osName.indexOf("mac") != -1) {
-			isMac = true;
-			defaultBrowser = "safari";
-		}
-		defaultBrowser = "chrome"; // set it as default for now.
-	}
-	Properties props = new Properties();
-	public final static String DRIVER_PATH = System.getProperty("selenium.drivers", "lib");
-	public final static String FIREFOX_DRIVER_PATH = System.getProperty("webdriver.gecko.driver",
-			DRIVER_PATH + File.separator + "geckodriver" + execExtn);
-	public final static String CR_DRIVER_PATH = System.getProperty("webdriver.chrome.driver",
-			DRIVER_PATH + File.separator + "chromedriver" + execExtn);
-	public final static String IE_DRIVER_PATH = System.getProperty("webdriver.ie.driver",
-			DRIVER_PATH + File.separator + "IEDriverServer" + execExtn);
-	public final static String IE_EDGE_DRIVER_PATH = System.getProperty("webdriver.ie.driver",
-			DRIVER_PATH + File.separator + "MicrosoftWebDriver" + execExtn);
+//			tr.addResultForTestCase("1", "1", tr.TEST_CASE_PASSED_STATUS, "");
+//		} else {
+//			tr.addResultForTestCase("1", "1", tr.TEST_CASE_PASSED_STATUS, "");
+//
+//		}
 
-	public final static long WAIT_TIME = Long.parseLong(System.getProperty("wait.time", "60")); // secs
-	public static String TEST_TENANT = null;
-	private static long TIMEOUT;
-	public static String DEFAULT_USER_NAME = null;
-	public static String DEFAULT_PASSWRD = null;
-	public static String BOX_USER = null;
-	public static String BOX_PASSWRD = null;
-	private static final String TEST_PROPERTY_FILE = "test.properties";
-	public static String AWS_AKEY = null;
-	public static String API_HOST = null;
-	public static String AWS_PKEY = null;
-	public static String SF_PASSWORD = null;
-	public static String SF_USER = null;
-	public String browser = System.getProperty("browser", defaultBrowser);
-
-	/*
-	 * 
-	 */
-	public AppTest() {
-		// Load Test Properties
-		// From File or system
-		props = PropsHandler.getProperties(TEST_PROPERTY_FILE);
-		APP_BASE_URL = props.getProperty("app.base.url", "http://localhost:8080/WebApp");
-		TEST_TENANT = props.getProperty("tenant.name", "KeyTech");
-		TIMEOUT = new Integer(props.getProperty("time.out", "25")).intValue();
-		DEFAULT_USER_NAME = props.getProperty("user.name", "testuser1");
-		DEFAULT_PASSWRD = props.getProperty("user.password", "testuser123");
-
-		API_HOST = props.getProperty("api.host", "http://localhost:8080");
-		logger.info("***** UITestHAndler: APP_BASE_URL " + AppTest.APP_BASE_URL);
 	}
 
-	/**
-	 * Open the new browser window by creating the web driver
-	 * 
-	 * @param browser
-	 * @param driverPath
-	 * @return
-	 */
-	public WebDriver openBrowser(String browser) {
-		if (FF_BROWSER.equals(browser)) {
-			logger.info("Opening " + FF_BROWSER);
-			System.setProperty("webdriver.gecko.driver", FIREFOX_DRIVER_PATH);
-			driver = new FirefoxDriver();
-			DesiredCapabilities caps = new DesiredCapabilities().firefox();
-			caps.setCapability("ignoreProtectedModeSettings", true);
-			caps.setCapability("ignoreZoomSetting", true);
-			caps.setCapability("draggable", true);
-			caps.setCapability("requireWindowFocus", true);
-		} else if (CR_BROWSER.equals(browser)) {
-			logger.info("Opening " + CR_BROWSER);
-			System.setProperty("webdriver.chrome.driver", CR_DRIVER_PATH);
-			DesiredCapabilities caps = new DesiredCapabilities().chrome();
-			caps.setCapability("ignoreProtectedModeSettings", true);
-			caps.setCapability("ignoreZoomSetting", true);
-			caps.setCapability("draggable", true);
-			caps.setCapability("requireWindowFocus", true);
-			caps.setCapability("chrome.switches", Arrays.asList("--incognito"));
-			driver = new ChromeDriver();
-		} else if (IE_BROWSER.equals(browser)) {
-			logger.info("Opening " + IE_BROWSER);
-			System.setProperty("webdriver.ie.driver", IE_DRIVER_PATH);
-			DesiredCapabilities caps = new DesiredCapabilities().internetExplorer();
-			// caps.setCapability("ignoreProtectedModeSettings", true);
-			caps.setCapability("ignoreZoomSetting", true);
-			caps.setCapability("draggable", true);
-			caps.setCapability("requireWindowFocus", true);
-			driver = new InternetExplorerDriver();
-		} else if (IE_EDGE_BROWSER.equals(browser)) {
-			logger.info("Opening " + IE_EDGE_BROWSER);
-			System.setProperty("webdriver.edge.driver", IE_EDGE_DRIVER_PATH);
-			driver = new EdgeDriver();
-		} else if (SF_BROWSER.equals(browser)) {
-			logger.info("Opening " + SF_BROWSER);
-			driver = new SafariDriver();
-		} else if (OP_BROWSER.equals(browser)) {
-			logger.info("Opening " + OP_BROWSER);
-			driver = new OperaDriver();
-		} else if (HU_BROWSER.equals(browser)) {
-			logger.info("Opening " + HU_BROWSER);
-			driver = new HtmlUnitDriver();
-		} else {
-			logger.info("Opening default " + FF_BROWSER);
-			driver = new FirefoxDriver();
+	/* TestCaseId - C19003 */
+	@Test(enabled = true, priority = 2, dependsOnMethods = "landingWelcomePage")
+	public void SidebarMenuOptions() {
+
+		logger.info("Getting Dashboard Icon");
+		List<WebElement> sideBarLogo = webAppHelper
+				.findElementsByXPath("//*[local-name()='svg' and (@class='c-sidebar-nav-icon')]");// .get(1)
+		// .getAttribute("xmlns");
+		for (int i = 0; i < sideBarLogo.size(); i++) {
+
+			logger.info("Asserting OTS By Date Icon");
+			Assert.assertTrue(sideBarLogo.get(i).getAttribute("xmlns").contains("http://www.w3.org/2000/svg"));
+
 		}
-		return driver;
+
+		logger.info("Getting Dashboard name");
+		String dashboardName = webAppHelper.findElementByXPath("//a[contains(@class,'c-sidebar-nav-link')]").getText();
+
+		logger.info("Asserting Dashboard Name");
+		Assert.assertTrue(dashboardName.contains("Dashboard"));
+
+		logger.info("Getting Reports name and Icon");
+		String reports = webAppHelper.findElementsByClassName("c-sidebar-nav-dropdown-toggle").get(0).getText();
+		String wms = webAppHelper.findElementsByClassName("c-sidebar-nav-dropdown-toggle").get(1).getText();
+
+		logger.info("Asserting Side Bar menu option Icon Text");
+		Assert.assertEquals(reports, "Reports");
+		Assert.assertEquals(wms, "WMS");
+
+	}
+
+	/* TestCaseId - C18995 */
+	@Test(enabled = false, priority = 3, dependsOnMethods = "SidebarMenuOptions")
+	public void headerTitleName()  {
+
+		logger.info("Getting Header title Names");
+		String DashboardText = webAppHelper.findElementByXPath("//li[@role='presentation']").getText();
+		String clientName = webAppHelper.findElementsByXPath("//a[contains(@class,'pl-1')]").get(1).getText().trim();
+
+		logger.info("Asserting Header title Names");
+		Assert.assertEquals(DashboardText, "Dashboard");
+		Assert.assertEquals(clientName, "JR365");
+
+	}
+
+	/* TestCaseId - C19001 */
+	@Test(enabled = true, priority = 4, dependsOnMethods = "landingWelcomePage")
+	public void sideBarLogoTest() {
+
+		logger.info("Getting side bar logo path");
+		String sideBarLogo = webAppHelper.findElementsByXPath("//a[contains(@class , 'c-sidebar-brand ')]//child::img")
+				.get(0).getAttribute("src");
+
+		logger.info("Asserting side bar Logo");
+		Assert.assertTrue(sideBarLogo.contains("logo-light"));
+
+		logger.info("Getting Side bar logo css value");
+		String logoCssValue = webAppHelper.findElementByClassName("c-sidebar-brand-full").getCssValue("vertical-align");
+
+		logger.info("Asserting Logo css value");
+		Assert.assertEquals(logoCssValue, "middle");
+	}
+
+	/* TestCaseId - C19000 */
+	@Test(enabled = true, priority = 5)
+	public void headerToggleIconTest() {
+
+		logger.info("Getting header toggle icon class name");
+		String headerToggleIconCSS = webAppHelper
+				.findElementByXPath("//*[contains(@class , 'c-sidebar-brand')]//parent::div").getAttribute("class");
+
+		logger.info("Asserting header toggle icon class name");
+		Assert.assertTrue(headerToggleIconCSS.contains(" c-sidebar-show"));
+
+		logger.info("Clicking Header toggle icon");
+		webAppHelper.findElementByXPath("//button[contains(@class , 'd-md-down-none')]//child::span").click();
+
+		logger.info("Getting header toggle icon class name");
+		String changedHeaderToggleIconCSS = webAppHelper
+				.findElementByXPath("//*[contains(@class , 'c-sidebar-brand')]//parent::div").getAttribute("class");
+
+		logger.info("Asserting changed header toggle icon class name");
+		Assert.assertTrue(changedHeaderToggleIconCSS.contains("c-sidebar c-sidebar-dark c-sidebar-fixed"));
+
+		logger.info("Clicking Header toggle icon");
+		webAppHelper.findElementByXPath("//button[contains(@class , 'd-md-down-none')]//child::span").click();
+
 	}
 
 	
-   @org.testng.annotations.Test(enabled=true)
-    public void shouldAnswerWithTrue()
-    {
- 
-	   openBrowser("chrome");
-	   //Maximize the browser  
-       driver.manage().window().maximize();  
 
-	   driver.navigate().to("https://dev-losan-d.aims360runway.com/");
-	   System.out.println("Login page open successfully");
-       
-              
-    }
+	/* TestCaseId - C19005 */
+	@Test(enabled = true, priority = 10) 
+	public void NavigateToOTSByDate()  {
+
+		logger.info("clicking reports");
+
+		webAppHelper.findElementByXPath("//*[text()='Reports']").click();
+
+		logger.info("clicking Ots By Date ");
+		webAppHelper.findElementByXPath("//a[text()='OTS By Date']").click();
+
+		logger.info("getting side nav title name Dashboard");
+		String dashboard = webAppHelper.findElementByXPath("//*[contains(@class,'badge-info')]//parent::a").getText();
+
+		logger.info("Asserting of OTS Date card");
+		Assert.assertTrue(dashboard.contains("Dashboard"));
+
+		logger.info("clicking  side nav title name Dashboard");
+		webAppHelper.findElementByXPath("//*[contains(@class,'badge-info')]//parent::a").click();
+
+//		if (dashboard.contains("Dashboard")) {
+//
+//			tr.addResultForTestCase("1", "3", tr.TEST_CASE_PASSED_STATUS, "");
+//		} else {
+//			tr.addResultForTestCase("1", "3", tr.TEST_CASE_FAILED_STATUS, "");
+//
+//		}
+	}
+
+	/* TestCaseId - C19022 */
+	@Test(enabled = false, priority = 11)
+	public void footerElements() {
+
+		logger.info("Getting Icon and Version number");
+		String versionText = webAppHelper.findElementByClassName("//span[@role='button']").getText();
+		String svgIcon = webAppHelper.findElementByXPath("//span[@role='button']/*[local-name()='svg']")
+				.getAttribute("xmlns");
+
+		logger.info("Asserting version Text and Svg Icon image");
+		Assert.assertEquals(versionText, "V2022.11.10");
+		Assert.assertTrue(svgIcon.contains("http://www.w3.org/2000/svg"));
+
+		logger.info("Getting Default footer work");
+		String defaultFooter = webAppHelper.findElementByXPath("//span[@role='button']/*[local-name()='svg']")
+				.getAttribute("class");
+
+		logger.info("Asserting Default footer in page");
+		Assert.assertTrue(defaultFooter.contains("pin"));
+
+		logger.info("Getting company name and Icon");
+		String companyName = webAppHelper.findElementsByClassName("mfs-auto").get(0).getText();
+//		String companyIcon = webAppHelper
+//				.findElementByXPath("//div[@class='mfs-auto']//child::span/*[local-name()='svg']")
+//				.getAttribute("xmlns");
+
+		logger.info("Asserting Company name and Icon");
+		Assert.assertTrue(companyName.contains(" JOY RIDE 365 LLC dba THINK ROYLN"));
+
+		logger.info("Getting Database name and Icon class name");
+		String databaseIcon = webAppHelper.findElementByXPath("//div[@class='mfs-auto']//child::i")
+				.getAttribute("class");
+		String databaseName = webAppHelper.findElementsByClassName("mfs-auto").get(0).getText();
+		logger.info("Asserting Database name and Icon");
+		Assert.assertEquals(databaseIcon, "fas fa-database");
+		Assert.assertTrue(databaseName.contains("JR365Data1"));
+
+		logger.info("Getting copyright Name in footer");
+		String copyRightName = webAppHelper.findElementsByClassName("mfs-auto").get(1).getText();
+
+		logger.info("Asserting copyright Name");
+		Assert.assertEquals(copyRightName, "© 2022 AIMS360");
+
+	}
+
+	@AfterClass
+	public void unsetUp() {
+		webAppHelper.quitBrowser();
+	}
+
+
+	
 }
