c0a6492e0a4b344d6a6297bb25a917f24adbed91
KamsaniAkhil <akhilkamsani01@gmail.com>
2022-12-16 20:00:48 +0530

    first commit

---
+++ src/main/java/BaseTesting/SampleTest/App.java
@@ -0,0 +1,16 @@
+package BaseTesting.SampleTest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	@Test(enabled =true)
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
